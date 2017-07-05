f5fa97b2bc64936105b19c3119316e13b36097f8
Andrew Maidah <a.maidah@gmail.com>
2017-07-04 20:51:32 -0700

    adds pointless @queries for superscript title prettification on mobile vs desktop
    Signed-off-by: Andrew Maidah <a.maidah@gmail.com>

--- react-ui/src/components/Nav/Nav.js
+++ react-ui/src/components/Nav/Nav.js
@@ -7,7 +7,10 @@ const Nav = () => {
   return (
     <div className="nav-wrapper">
       <div className="container">
-        <h4 className="logo">Partial Refund Calculator <sup>4 BigCommerce!</sup></h4>
+        <h4 className="logo">Partial Refund Calculator
+          <sup className="hide-on-small"> for BigCommerce!</sup>
+          <sup className="show-on-small-only"> for BC!</sup>
+        </h4>
       </div>
     </div>
   )
